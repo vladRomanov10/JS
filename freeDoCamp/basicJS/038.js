e99b9573154bd1c98e95fb78ad9fe4b053bf0320
Vlad Romanov <vladromeur@gmail.com>
2023-11-07 13:02:58 +0600

    Выполнил задания 034 - 038

---
+++ freeDoCamp/basicJS/038.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myNoun + " is so " + myAdjective + " but it " + myVerb + " so " + myAdverb + "."; // Change this line
+// Only change code above this line
