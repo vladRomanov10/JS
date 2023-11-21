e6fa2eefbcd3d6079e3bcc58345870e34d0cec27
Vlad Romanov <vladromeur@gmail.com>
2023-11-21 18:35:33 +0600

    Пытаюсь решить задачу на большее и меньшее число в строке

---
+++ codeWars/7kyu/highestAndLowest.js
@@ -0,0 +1,22 @@
+function highAndLow(numbers) {
+    const numbersWithoutSpaces = numbers.replaceAll(' ', ''); //Проблема с переводом строковых двухначных и более цифр
+    const numbersArray = Array.from(numbersWithoutSpaces); // в массив как числа, число 10 переводится как 1 и 0
+    const comparisonNumber = numbersArray.length - 1;
+    let numOfAdv = 0;
+    let theBiggestNum = 0;
+    let theLowestNum = 0;
+    for (let i = 0; i < numbersArray.length; i++) {
+        for (let z = 0; z < numbersArray.length; z++) {
+            if (numbersArray[i] > numbersArray[z]) {
+                numOfAdv++;
+            }
+        }
+        if (numOfAdv == comparisonNumber) {
+            theBiggestNum = numbersArray[i]
+        } else if (numOfAdv == 0) {
+            theLowestNum = numbersArray[i]
+        }
+        numOfAdv = 0;
+    }
+    return theBiggestNum.toString() + ' ' + theLowestNum.toString();
+}
