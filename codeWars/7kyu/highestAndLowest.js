28699b73c8304c2ccac1e85a045eadde2a31542b
Vlad Romanov <vladromeur@gmail.com>
2023-11-22 12:06:45 +0600

    Решение задачки по поиску наибольшего и наименьшего числа с лютейшим говнокодом

--- codeWars/7kyu/highestAndLowest.js
+++ codeWars/7kyu/highestAndLowest.js
@@ -1,13 +1,27 @@
 function highAndLow(numbers) {
-    const numbersArrayStr = numbers.split(' ');
-    let numbersArray = []; 
-    for (let i = 0; i < numbersArrayStr.length; i++) {
-        numbersArray.push(Number(numbersArrayStr[i]));
+    const numbersArrayStr = numbers.split(' '); // Делаю из строки массив с строками
+    const nonRepeatNumStr = new Set(numbersArrayStr); // Удалаяю с помошью new Set повтрояющиеся элементы
+    const nonRepeatNumArrayStr = Array.from(nonRepeatNumStr); // Делаю из коллекции массив с строками
+    let numbersArray = [];
+    for (let i = 0; i < nonRepeatNumArrayStr.length; i++) { 
+        numbersArray.push(Number(nonRepeatNumArrayStr[i])); // Перевожу массив из строк в цифры
+    }
+    let theBiggestNum = 0;
+    let theLowestNum = 0;
+    if (numbersArray.length == 1) {
+        theBiggestNum = numbersArray[0];
+        theLowestNum = numbersArray[0];
+        return theBiggestNum.toString() + ' ' + theLowestNum.toString();
+    }
+    if (numbersArray.length == 2) {
+        if (numbersArray[0] == numbersArray[1]) {
+            theBiggestNum = numbersArray[0];
+            theLowestNum = numbersArray[0];
+            return theBiggestNum.toString() + ' ' + theLowestNum.toString();
+        }
     }
     const comparisonNumber = numbersArray.length - 1;
     let numOfAdv = 0;
-    let theBiggestNum = 0;
-    let theLowestNum = 0;
     for (let i = 0; i < numbersArray.length; i++) {
         for (let z = 0; z < numbersArray.length; z++) {
             if (numbersArray[i] > numbersArray[z]) {
@@ -23,30 +37,3 @@ function highAndLow(numbers) {
     }
     return theBiggestNum.toString() + ' ' + theLowestNum.toString();
 }
-//
-function highAndLow(numbers) {
-    numbersArrayStr = numbers.split(' ');
-    numbersArray = []; 
-    for (let i = 0; i < numbersArrayStr.length; i++) {
-        numbersArray.push(Number(numbersArrayStr[i]));
-    }
-    comparisonNumber = numbersArray.length - 1;
-    numOfAdv = 0;
-    theBiggestNum = 0;
-    theLowestNum = 0;
-    for (i = 0; i < numbersArray.length; i++) {
-        for (z = 0; z < numbersArray.length; z++) {
-            if (numbersArray[i] > numbersArray[z]) {
-                numOfAdv++;
-            }
-        }
-        if (numOfAdv == comparisonNumber) {
-            theBiggestNum = numbersArray[i]
-        } else if (numOfAdv == 0) {
-            theLowestNum = numbersArray[i]
-        }
-        numOfAdv = 0;
-    }
-    return theBiggestNum.toString() + ' ' + theLowestNum.toString();
-}
-highAndLow("8 8");
