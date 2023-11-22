7d07fac003602a7a5251c39391b987f129aaba46
Vlad Romanov <vladromeur@gmail.com>
2023-11-22 11:03:20 +0600

    Состояние задачки до говнокода

--- codeWars/7kyu/highestAndLowest.js
+++ codeWars/7kyu/highestAndLowest.js
@@ -1,7 +1,10 @@
 function highAndLow(numbers) {
-    const numbersWithoutSpaces = numbers.replaceAll(' ', ''); //Проблема с переводом строковых двухначных и более цифр
-    const numbersArray = Array.from(numbersWithoutSpaces); // в массив как числа, число 10 переводится как 1 и 0
-    const comparisonNumber = numbersArray.length - 1; // нужно разбить строку через str.split(' ')
+    const numbersArrayStr = numbers.split(' ');
+    let numbersArray = []; 
+    for (let i = 0; i < numbersArrayStr.length; i++) {
+        numbersArray.push(Number(numbersArrayStr[i]));
+    }
+    const comparisonNumber = numbersArray.length - 1;
     let numOfAdv = 0;
     let theBiggestNum = 0;
     let theLowestNum = 0;
@@ -20,3 +23,30 @@ function highAndLow(numbers) {
     }
     return theBiggestNum.toString() + ' ' + theLowestNum.toString();
 }
+//
+function highAndLow(numbers) {
+    numbersArrayStr = numbers.split(' ');
+    numbersArray = []; 
+    for (let i = 0; i < numbersArrayStr.length; i++) {
+        numbersArray.push(Number(numbersArrayStr[i]));
+    }
+    comparisonNumber = numbersArray.length - 1;
+    numOfAdv = 0;
+    theBiggestNum = 0;
+    theLowestNum = 0;
+    for (i = 0; i < numbersArray.length; i++) {
+        for (z = 0; z < numbersArray.length; z++) {
+            if (numbersArray[i] > numbersArray[z]) {
+                numOfAdv++;
+            }
+        }
+        if (numOfAdv == comparisonNumber) {
+            theBiggestNum = numbersArray[i]
+        } else if (numOfAdv == 0) {
+            theLowestNum = numbersArray[i]
+        }
+        numOfAdv = 0;
+    }
+    return theBiggestNum.toString() + ' ' + theLowestNum.toString();
+}
+highAndLow("8 8");
