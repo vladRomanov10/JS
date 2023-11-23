1147ecc66592c4b4bd265014b880fccc6d976fc5
Vlad Romanov <vladromeur@gmail.com>
2023-11-23 14:10:32 +0600

    Пытаюсь решить задачу на возврат самого большого числа

---
+++ codeWars/7kyu/descendingOrder.js
@@ -0,0 +1,60 @@
+function descendingOrder(n){
+    let numbers = n;
+    let numArray = [];
+    let numDescendingArray = [];
+    let moreThan = 0;
+    let repeatingNum = 0;
+    if (numbers == 0) {
+        return 0;
+    }
+    while (numbers >= 1) { //Нужно пересобрать часть кода, где число преобразуется в массив
+        numArray.unshift(numbers % 10);//сейчас проблема с числом 01, оно не преобразуется в
+        numbers = Math.floor(numbers / 10);// 0 и 1, а делается как 1
+    }
+    if (numbers === 0) {
+        numArray.push(0);
+    }
+    for (let i = 0; i < numArray.length; i++) {
+        for (let z = 0; z < numArray.length; z++) {
+            if (numArray[i] > numArray[z]) {
+                moreThan++;
+            }
+        }
+        if (numDescendingArray [(numArray.length - 1) - moreThan] == numArray[i]) {
+            repeatingNum++;
+            numDescendingArray [(numArray.length - 1) - (moreThan + repeatingNum)] = numArray[i];
+        }
+        numDescendingArray [(numArray.length - 1) - moreThan] = numArray[i];
+        moreThan = 0;
+    }
+    return +numDescendingArray.join('');
+}
+//------
+function descendingOrder(n){
+    numbers = n;
+    numArray = [];
+    numDescendingArray = [];
+    moreThan = 0;
+    repeatingNum = 0;
+    if(numbers == 0) {
+        return 0;
+    }
+    while (numbers >= 1) {
+        numArray.unshift(numbers % 10);
+        numbers = Math.floor(numbers / 10);
+    }
+    for (i = 0; i < numArray.length; i++) {
+        for (z = 0; z < numArray.length; z++) {
+            if (numArray[i] > numArray[z]) {
+                moreThan++;
+            }
+        }
+        if (numDescendingArray [(numArray.length - 1) - moreThan] == numArray[i]) {
+            repeatingNum++;
+            numDescendingArray [(numArray.length - 1) - (moreThan + repeatingNum)] = numArray[i];
+        }
+        numDescendingArray [(numArray.length - 1) - moreThan] = numArray[i];
+        moreThan = 0;
+    }
+    return +numDescendingArray.join('');
+}
