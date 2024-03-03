f397abfc3a6a0ad60da99950af82c1d3d32ef6f0
Vlad Romanov <vladromeur@gmail.com>
2024-03-03 14:39:12 +0500

    Примеры из курса Богдана

--- JavaScriptBogdanStaschuk/randomeCode/await.js
+++ JavaScriptBogdanStaschuk/randomeCode/await.js
@@ -2,4 +2,12 @@ const timerPromise = () =>
     new Promise((resolve, reject) =>
         setTimeout(() => {
             return resolve()
-        }, 20000));
+        }, 2000));
+
+const asyncFn = async () => {
+    console.log('Timer starts')
+    await timerPromise()
+    console.log('Timer ended')
+}
+
+asyncFn()
